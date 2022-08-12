c4ba6ae5c03df78fd9b406ec8bddd4998ea4af9c
Yossef Jesus Hernandez Rodriguez <yossef.her25@gmail.com>
2022-08-12 00:42:02 -0500

    css missing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [line1, setLine1] = useState('')
+  const [line2, setLine2] = useState('')
+  const [image, setImage] = useState('')
+
+  const onChangeLine1 = function (value){
+    setLine1(value.target.value)
+  }
+  const onChangeLine2 = function (value){
+    setLine2(value.target.value)
+  }
+  const setImg = function(value){
+    setImage(value.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={setImg}>
+        <option value="1.jpg">Pepe in fire</option>
+        <option value="2.jpg">Pepe thinking</option>
+        <option value="3.jpg">Shrek random</option>
+        <option value="4.jpg">CSGO img random</option>
+        <option value="5.jpg">Pepe beautiful</option>
+        <option value="6.jpg">Sponge Bob lul</option>
+      </select> <br/>
+      
+      <input onChange={onChangeLine1} type="text" placeholder='Text Top'/> <br/>
+      <input onChange={onChangeLine2} type="text" placeholder='Text Bottom'/> <br/>
+      <button>Export</button>
+
+      <div>
+        <span>{line1}</span> <br/>
+        <span>{line2}</span>
+        <img src={"/Images/" + image }/>
+      </div>
+
     </div>
   );
 }
